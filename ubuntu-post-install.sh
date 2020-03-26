9254f74566583fe90afa2182f8aa0db2f8b1e35f
Thomas Gohard <thomasgohard@users.noreply.github.com>
2020-03-26 18:19:40 -0400

    Configure git to use libsecret

--- ubuntu-post-install.sh
+++ ubuntu-post-install.sh
@@ -2,5 +2,7 @@
 sudo apt update
 sudo apt -y upgrade
 sudo apt -y full-upgrade
-sudo apt -y install git
+sudo apt -y install git libsecret-1-0 libsecret-1-dev
 mkdir ~/git
+(cd /usr/share/doc/git/contrib/credential/libsecret && sudo make)
+git config --global credential.helper /usr/share/doc/git/contrib/credential/libsecret/git-credential-libsecret
