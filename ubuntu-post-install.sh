f3c8b28e1cc6045f98d7fec3d5d1ffe5d8ed073f
Thomas Gohard <thomasgohard@users.noreply.github.com>
2020-03-26 12:07:40 -0400

    Add Visual Studio Code

--- ubuntu-post-install.sh
+++ ubuntu-post-install.sh
@@ -3,6 +3,7 @@ sudo apt update
 sudo apt -y upgrade
 sudo apt -y full-upgrade
 sudo apt -y install git libsecret-1-0 libsecret-1-dev
+sudo snap install --classic code
 mkdir ~/git
 (cd /usr/share/doc/git/contrib/credential/libsecret && sudo make)
 git config --global credential.helper /usr/share/doc/git/contrib/credential/libsecret/git-credential-libsecret
