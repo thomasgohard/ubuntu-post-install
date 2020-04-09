09451ce7b163e3c7f9d0de3bb30f6143770ea563
Thomas Gohard <thomasgohard@users.noreply.github.com>
2020-04-09 19:29:20 -0400

    Add Vagrant

--- ubuntu-post-install.sh
+++ ubuntu-post-install.sh
@@ -11,7 +11,7 @@ sudo add-apt-repository "deb [arch=amd64] https://download.virtualbox.org/virtua
 sudo apt update
 sudo apt -y upgrade
 sudo apt -y full-upgrade
-sudo apt -y install git libsecret-1-0 libsecret-1-dev jq moreutils apt-transport-https virtualbox-6.0
+sudo apt -y install git libsecret-1-0 libsecret-1-dev jq moreutils apt-transport-https virtualbox-6.0 vagrant
 sudo apt update
 sudo apt -y install dotnet-sdk-3.1
 sudo snap install --classic code
@@ -20,6 +20,7 @@ jq -r '. + {"telemetry.enableCrashReporter": false}' $VSCODE_SETTINGS | sponge $
 jq -r '. + {"telemetry.enableTelemetry": false}' $VSCODE_SETTINGS | sponge $VSCODE_SETTINGS
 echo "export DOTNET_CLI_TELEMETRY_OPTOUT=1" | sudo tee /etc/profile.d/dotnet-telemetry.sh
 mkdir ~/git
+mkdir ~/vagrant
 (cd /usr/share/doc/git/contrib/credential/libsecret && sudo make)
 git config --global credential.helper /usr/share/doc/git/contrib/credential/libsecret/git-credential-libsecret
 rm packages-microsoft-prod.deb
