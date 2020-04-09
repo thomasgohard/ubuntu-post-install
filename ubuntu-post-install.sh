209fd61876a149d6e86be9b2fe3d2a175b6cd37f
Thomas Gohard <thomasgohard@users.noreply.github.com>
2020-04-09 13:03:02 -0400

    Add GitHub Pages gem for local testing

--- ubuntu-post-install.sh
+++ ubuntu-post-install.sh
@@ -21,3 +21,4 @@ mkdir ~/git
 (cd /usr/share/doc/git/contrib/credential/libsecret && sudo make)
 git config --global credential.helper /usr/share/doc/git/contrib/credential/libsecret/git-credential-libsecret
 rm packages-microsoft-prod.deb
+gem install github-pages
